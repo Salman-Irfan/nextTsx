97b95e884561a0e6a4bbd695e04ac378a8754727
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-14 17:05:21 +0500

    use state with user form data

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-const page = () => {
-    return <div>contact page</div>;
+interface Person {
+    name: string;
+    age: number;
+}
+
+const Page = () => {
+    const [user, setUser] = useState<Person>({ name: "", age: 0 });
+
+    const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser((prev) => ({
+            ...prev,
+            age: Number(e.target.value),
+        }));
+    };
+
+    console.log(user);
+
+    return (
+        <>
+            <h1>Contact page</h1>
+            <form>
+                <input
+                    className="border-4"
+                    type="number"
+                    name="age"
+                    id="age"
+                    placeholder="Age"
+                    value={user.age}
+                    onChange={handleAgeChange}
+                />
+            </form>
+        </>
+    );
 };
 
-export default page;
+export default Page;
