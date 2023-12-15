3b61711da252ecac46cbf24279d8415962657e47
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-15 09:32:57 +0500

    navbar spelling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/layouts/Navbat'
+import Navbar from '@/layouts/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
